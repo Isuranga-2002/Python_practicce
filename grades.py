84abbbaacebf7c16cc0668a3362491df22994961
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-10-17 12:32:29 +0530

    Add grade evaluation script

---
+++ grades.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+grade = int(input("Enter your grade (0-100): "))
+
+if 55 <= grade <= 100:
+  print("You passed.")
+elif 0 <= grade < 55:
+  print("You failed.")
+else:
+  print("Invalid grade.")
