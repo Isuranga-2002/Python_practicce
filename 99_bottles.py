caf98e4e4ff70eec26e6c937159e3f9bc252535a
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-10-18 14:46:42 +0530

    Add implementation of the 99 Bottles of Beer song

---
+++ 99_bottles.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+for i in range(1,100):
+    print(f"{100-i} bottles of beer on the wall")
+    print(f"{100-i} bottles of beer")
+    print(f"Take one down and pass it around")
+    print(f"{99-i} bottles of beer on the wall")
+    print("\n")
