7d30bed41b1b096add5c4a17b8fc6f70b9cf8c5c
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-10-19 18:55:47 +0530

    Add initial implementation of basic arithmetic operations

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(a,b):
+    answer = a + b
+    return answer
+
+def substract(a,b):
+    answer = a - b
+    return answer
+
+def multiply(a,b):
+    answer = a * b
+    return answer
+
+def divide(a,b):
+    answer = a / b
+    return answer
+
+def exp(a,b):
+    answer = a ** b
+    return answer
+
+print(add(3,4)) #7
+print(substract(5,2)) #3
+print(multiply(7,8)) #56
+print(divide(9,3)) #3
+print(exp(2,2)) #4
