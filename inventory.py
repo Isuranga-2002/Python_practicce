5e080ff117abf88460328b653963fb4d1102eee9
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-10-19 12:36:45 +0530

    Add initial implementation of book list management

---
+++ inventory.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+
+book_list = ['Harry Potter',
+'1984',
+'The Fault in Our Stars',
+'The Mom Test',
+'Life in Code']
+
+book_list.append('Pachinko')
+book_list.remove('The Fault in Our Stars')
+book_list.pop(1)
+
+print(book_list)
