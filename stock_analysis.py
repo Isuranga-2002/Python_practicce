6e09c8c1ef8963e7911e8222f39872ea3f19df5b
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2025-10-20 13:00:20 +0530

    Fix index errors in stock price functions and update print statements for clarity

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,10 +1,18 @@
-stock_prices = [34.68, 36.09, 34.94, 33.97, 34.68, 35.82, 43.41, 44.29, 44.65, 53.56, 49.85, 48.71, 48.71, 49.94, 48.53, 47.03, 46.59, 48.62, 44.21, 47.21]
+stock_prices = [34.68, 36.09, 34.02, 33.97, 34.68, 35.82, 43.41, 44.29, 44.65, 53.56, 49.85, 48.71, 48.71, 49.94, 48.53, 47.03, 46.59, 48.62, 44.21, 47.21]
 
 def price_at(x):
-    return stock_prices[x+1]
+    return stock_prices[x-1]
 
 def max_price(a,b):
-    return max(stock_prices[a:b])
+    return max(stock_prices[a-1:b])
 
 def min_price(a,b):
-    return min(stock_prices[a:b])
+    return min(stock_prices[a-1:b])
+
+print("price of the 5th day is: ", price_at(5))
+
+print("Maximum within the period: ",max_price(1,20))
+
+print("Minimum within the period: ",min_price(1,20))
+
+
